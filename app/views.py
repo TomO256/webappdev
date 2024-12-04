e550e32cf155a9697e1e6ac0989718a81da76eff
TomO256 <github@whiskey.aleeas.com>
2024-12-04 17:55:57 +0000

    views.py compliance with pep8

--- app/views.py
+++ app/views.py
@@ -4,12 +4,15 @@ from flask_admin.contrib.sqla import ModelView
 from .forms import WelcomeForm, LoginForm, ArticleForm
 from .models import User, Article, Category
 from .Library import *
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-import logging, json
+from flask_login import LoginManager, login_user, logout_user
+from flask_login import login_required, current_user
+import logging
+import json
 
 logging.basicConfig(filename="log.log", encoding="utf-8", level=logging.DEBUG)
 
-# Below lines used for rendering admin view, potential security risk if uncommented
+# Below lines used for rendering admin view, potential
+# security risk if remains uncommented
 # admin.add_view(ModelView(User, db.session))
 # admin.add_view(ModelView(Article, db.session))
 # admin.add_view(ModelView(Category, db.session))
@@ -19,12 +22,13 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     '''
     Main page for the website, containg the username field.
@@ -71,29 +75,32 @@ def login(username=None):
 
 @app.route("/signup/", methods=["GET", "POST"])
 @app.route("/signup/<string:username>", methods=["GET", "POST"])
-def signup(username=None): 
+def signup(username=None):
     '''
-    Signup page for new users, passes through to the discover
-    page if credentials are new and unique.
+    Signup page for new users, passes through
+    to the discover page if credentials are new and unique.
     '''
     form = LoginForm(username=username)
     if form.validate_on_submit():
         logging.info("Validated signup form")
         if user_exists(form.username.data.upper()):
-            flash("The username chosen is taken. If this is you, please sign in.")
+            flash("""The username chosen is taken.
+                  If this is you, please sign in.""")
             return render_template("signup.html",
                                    title="Sign Up",
-                                   form=form,submit_message="Sign Up")
+                                   form=form, submit_message="Sign Up")
 
         hashed_password = hash_data(form.password.data)
-        user = User(username=form.username.data.upper(),password=hashed_password)
+        user = User(username=form.username.data.upper(),
+                    password=hashed_password)
         db.session.add(user)
         db.session.commit()
         logging.info("Redirected to base")
         return redirect("/")
     return render_template("signup.html",
                            title="Sign Up",
-                           form=form,submit_message="Sign Up")
+                           form=form, submit_message="Sign Up")
+
 
 @app.route("/discover")
 @login_required
@@ -102,27 +109,31 @@ def discover():
     Renders the main discover page - if the user is authenticated.
     Lists all the articles in order of number of likes.
     '''
-    return render_template("discover.html",
-                           title="Discover",
-                    username=current_user.username,
-                    all_articles=Article.query.order_by(Article.likes.desc()).all())
+    return render_template("discover.html", title="Discover",
+                           username=current_user.username,
+                           all_articles=Article.query.order_by
+                           (Article.likes.desc()).all())
+
 
 @app.route("/portfolio")
 @login_required
 def portfolio():
     '''
-    Renders the portfolio page - listing all the articles written by 
+    Renders the portfolio page - listing all the articles written by
     the user signed in. Requires the user to be signed in.
     '''
     user_id = get_id("user", current_user.username)
     user_articles = Article.query.filter_by(author_id=user_id).all()
     total_reactions = 0
     for article in user_articles:
-        total_reactions += article.likes + article.laughs + article.grimaces + article.blanks + article.surprises
+        total_reactions += (article.likes + article.laughs +
+                            article.grimaces + article.blanks +
+                            article.surprises)
     return render_template("portfolio.html",
                            title="Portfolio",
                            all_articles=user_articles,
-                           total_reactions = total_reactions)
+                           total_reactions=total_reactions)
+
 
 @app.route("/create", methods=["GET", "POST"])
 @login_required
@@ -136,15 +147,21 @@ def create():
         logging.info("Validated Article Form")
         status = validate_article(form.title.data, form.content.data)
         logging.info(status)
-        if status != True:
+        if not status:
             flash(status)
             return render_template("create.html",
-                           title="Write Article",
-                           form=ArticleForm(title=form.title.data, content=form.content.data, category=form.category.data,
-                                            submit_message="Create Article"))
+                                   title="Write Article",
+                                   form=ArticleForm(title=form.title.data,
+                                                    content=form.content.data,
+                                                    category=form.category.
+                                                    data,
+                                                    submit_message="""
+Create Article"""
+                                                    ))
         category_id = get_id("category", form.category.data)
         author_id = get_id("user", current_user.username)
-        article = Article(title=form.title.data, content=form.content.data, category_id=category_id, author_id=author_id)
+        article = Article(title=form.title.data, content=form.content.data,
+                          category_id=category_id, author_id=author_id)
         db.session.add(article)
         db.session.commit()
         return redirect("/portfolio")
@@ -153,6 +170,7 @@ def create():
                            title="Write Article",
                            form=form,
                            submit_message="Create Article")
+
 
 @app.route("/edit/<int:id>", methods=["GET", "POST"])
 @login_required
@@ -166,26 +184,34 @@ def edit(id):
     article = Article.query.get(id)
     if article.author_id != get_id("user", current_user.username):
         return redirect("/discover")
-    form=ArticleForm(title=article.title, content=article.content, category=article.category)
+    form = ArticleForm(title=article.title,
+                       content=article.content, category=article.category)
     if form.validate_on_submit():
         logging.info("Validated Article Form")
-        status = validate_article(form.title.data, form.content.data, article.title, article.content)
+        status = validate_article(form.title.data,
+                                  form.content.data,
+                                  article.title,
+                                  article.content)
         logging.info(status)
-        if status != True:
+        if not status:
             flash(status)
             return render_template("edit.html",
-                           title="Edit Article",
-                           form=ArticleForm(title=form.title.data, content=form.content.data, category=form.category.data),
-                                            submit_message="Edit Article")
+                                   title="Edit Article",
+                                   form=ArticleForm(title=form.title.data,
+                                                    content=form.content.data,
+                                                    category=form.category.
+                                                    data),
+                                   submit_message="Edit Article")
         article.title = form.title.data
         article.content = form.content.data
         article.category_id = get_id("category", form.category.data)
         db.session.commit()
         return redirect("/portfolio")
     return render_template("edit.html",
-                        title="Edit Article",
-                        form=form,
-                        submit_message="Edit Article")
+                           title="Edit Article",
+                           form=form,
+                           submit_message="Edit Article")
+
 
 @app.route("/delete/<int:id>", methods=["POST"])
 def delete(id):
@@ -201,6 +227,7 @@ def delete(id):
     db.session.commit()
     return redirect("/portfolio")
 
+
 @app.route("/view/<int:id>")
 @login_required
 def view(id):
@@ -209,8 +236,9 @@ def view(id):
     '''
     article = Article.query.get(id)
     return render_template("view_article.html",
-                           title = article.title,
-                           article = article)
+                           title=article.title,
+                           article=article)
+
 
 @app.route("/react", methods=["POST"])
 @login_required
@@ -235,9 +263,9 @@ def react():
         article.blanks += 1
     elif reaction == "surprises":
         article.surprises += 1
-    
+
     db.session.commit()
-    
+
     return json.dumps({
         "status": "OK",
         "likes": article.likes,
@@ -246,6 +274,7 @@ def react():
         "blanks": article.blanks,
         "surprises": article.surprises
     })
+
 
 @app.route("/quit")
 @login_required
